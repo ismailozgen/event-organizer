06aa21bd28ea2d26e11cd6268d33778b248c91ef
ismailozgen <ismail.ozgen@gmail.com>
2016-09-02 05:31:21 +0300

    test heroku

--- src/main/java/com/exercise/organizer/OrganizerApplication.java
+++ src/main/java/com/exercise/organizer/OrganizerApplication.java
@@ -14,8 +14,6 @@ public class OrganizerApplication extends SpringBootServletInitializer {
 	}
 	
 	public static void main(String[] args) {
-		
-		String s = "";
 		SpringApplication.run(OrganizerApplication.class, args);
 	}
 	
